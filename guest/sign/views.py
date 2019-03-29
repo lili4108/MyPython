de49d1479fe0b81fc036df4d21dbbba11572721d
Charlie's laptop <lizi4108@vip.qq.com>
2019-03-29 22:57:21 +0800

    update

--- guest/sign/views.py
+++ guest/sign/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
-#def index(request):
+# def index(request):
 #    return HttpResponse("Hello Django!")
 
 def index(request):
-	return render(request,"index.html")
+    return render(request, "index.html")
