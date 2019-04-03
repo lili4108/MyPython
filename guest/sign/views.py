595bdb6a744eb663f0422be72b46bc293ae9a0d5
Charlie's laptop <lizi4108@vip.qq.com>
2019-04-03 23:53:27 +0800

    updated 1

--- guest/sign/views.py
+++ guest/sign/views.py
@@ -1,30 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib import auth
+from django.contrib.auth.decorators import  login_required
 
 
 # Create your views here.
-# def index(request):
+# def index(request123):
 #    return HttpResponse("Hello Django!")
 
-def index(request):
-    return render(request, "index.html")
+def index(request123):
+    return render(request123, "index.html")
 
-def login_action(request):
-    if request.method == 'POST':
-        username = request.POST.get('username','')
-        password = request.POST.get('password','')
-        if username == 'admin' and password == 'admin123':
+def login_action(request123):
+    if request123.method == 'POST':
+        username = request123.POST.get('username','')
+        password = request123.POST.get('password','')
+        '''
+		if username == 'admin' and password == 'admin123':
             #return HttpResponseRedirect('/event_manage/')
             response = HttpResponseRedirect('/event_manage')
             #response.set_cookie('user',username,3600) # add cookie to browser
-            request.session['user'] = username
+            request123.session['user'] = username
             return response
+		'''
 
+        user = auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request123,user) # login
+            request123.session['user'] = username
+            response = HttpResponseRedirect('/event_manage/')
+            return response
+		
         else :
-            return render(request,'index.html',{'error':'username or password error!'})
+            return render(request123,'index.html',{'error':'username or password error!'})
 
-def event_manage(request):
-    #return render(request,"event_manage.html")
-    #username = request.COOKIES.get('user','')
-    username = request.session.get('user','')
-    return render(request,"event_manage.html",{"user":username})
+@login_required
+def event_manage(request123):
+    #return render(request123,"event_manage.html")
+    #username = request123.COOKIES.get('user','')
+    username = request123.session.get('user','')
+    return render(request123,"event_manage.html",{"user":username})
